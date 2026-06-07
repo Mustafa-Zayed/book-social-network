579220d9c36dbbccf19d093e5d488bc208a227fa
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-06-07 23:14:44 +0300

    - chore: bump backend version to 1.0.2 - chore: bump frontend version to 1.0.1

--- book-network-ui/src/app/components/register/register.ts
+++ book-network-ui/src/app/components/register/register.ts
@@ -39,7 +39,7 @@ export class Register {
       })
       .subscribe({
         next: () => {
-          this.router.navigate(['login']);
+          this.router.navigate(['activate-account']);
         },
         error: (err) => {
           this.errorMsg.set(err.error.validationErrors);
