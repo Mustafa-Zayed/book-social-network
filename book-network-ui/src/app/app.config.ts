be82634a60f622738be0ebbffedcbbf10060d0ff
Mustafa-Zayed <mostafa_31070285@ics.tanta.edu.eg>
2026-06-16 02:24:37 +0300

    Enhance the UI and use ngx-toastr

--- book-network-ui/src/app/app.config.ts
+++ book-network-ui/src/app/app.config.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 import { inject } from '@angular/core';
 import { routes } from './app.routes';
 import { tokenInterceptorProvider } from './services/interceptor/token.interceptor';
@@ -16,5 +17,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([tokenInterceptorProvider])),
     provideAppInitializer(() => inject(KeycloakService).init()),
+    provideToastr({
+      progressBar: true,
+      closeButton: true,
+      newestOnTop: true,
+      tapToDismiss: true,
+      positionClass: 'toast-bottom-right',
+      timeOut: 8000,
+    }),
   ],
 };
